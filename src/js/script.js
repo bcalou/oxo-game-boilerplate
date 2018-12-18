fa23b57fcea99646d6b8dff7079a5a2527d606e0
simtaxx <souici.haris@gmail.com>
2018-12-18 14:28:06 +0100

    random fireplace

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 var santaSpeed = 10;
-var stageSpeed = 10;
+var stageSpeed = 3;
 var direction = "left";
 var directionDown = "down";
-var gravity = 3;
+var gravity = 1;
 var size = 1;
 var santa;
 var fireplace;
@@ -13,7 +13,6 @@ var stageInterval;
 oxo.inputs.listenKeyOnce("enter", function() {
   if (oxo.screens.getCurrentScreen !== "game") {
     oxo.screens.loadScreen("game", function() {
-      fireplace = document.getElementById("fireplace");
       santa = document.getElementById("santa");
       ground = document.getElementById("ground");
       santaInterval = setInterval(player, santaSpeed);
@@ -25,14 +24,13 @@ oxo.inputs.listenKeyOnce("enter", function() {
   }
 });
 
-function jump() {
-  if (gravity > 0) {
-    gravity = -gravity;
-    setTimeout(function() {
-      gravity = -gravity;
-    }, 1000);
-  }
-}
+var randomFireplace = oxo.createElement({
+  class: 'stage_fireplace',
+  obstacle: true,
+  styles : {
+    transform: 'translate(50px, 50px)'
+  },
+});
 
 function player() {
   oxo.animation.move(santa, directionDown, gravity, true);
@@ -41,36 +39,7 @@ function stage() {
   oxo.animation.move(fireplace, direction, size, true);
 }
 
-function addFireplace() {
-  // Add a bonus element to the screen at a random position
-  var randomFireplace = oxo.elements.createElement({
-    class: ".stage__fireplace",
-    styles: {
-      transform:
-        'translate(' +
-        oxo.utils.getRandomNumber(0, firepl - 1) * size +
-        'px, '
-    },
-  });
-  oxo.elements.onCollisionWithElementOnce(santa, randomFireplace, function() {
-    console.log('newFireplaceCollisioned')
-  });
-}
-
-/* function randomFireplace() {
-  var fireplace = oxo.elements.createElement({
-    styles: {
-      transform:
-        'translate(' +
-        oxo.utils.getRandomNumber(0, xSquares - 1) * size +
-        'px, ' +
-        oxo.utils.getRandomNumber(0, ySquares - 1) * size +
-        'px)',
-    }
-  });
-}
-*/ 
 
 oxo.inputs.listenKey("up", function() {
-  jump();
+  oxo.animation.move(santa, "up", 100, true);
 });
