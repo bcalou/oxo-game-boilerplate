051d57399d309f34ef275e9fcb401aaafbe1c6ca
Pierre Secher <pierresecher98@gmail.com>
2018-12-20 11:26:42 +0100

    continue game

--- src/js/script.js
+++ src/js/script.js
@@ -5,82 +5,173 @@ oxo.inputs.listenKeyOnce('enter', function() {
   }
 });
 
-var gravity = 70;  //Speed of falling
-var time;
+// var rebound = 300;         //height in pixels of players rebounds
+var ySpeed1 = 50;             //gravity strength in pixels per [gameSpeed]
+var ySpeed2 = 50;
+var gameSpeed = 10;           //game speed
+var movementSpeed = 10;       //left and right player speed
+var platformSpawning = 750;   //speed of new platforms spawning
+var gravity = 10;
 var flying = true;
+
+function destroyObj(obj) {
+obj = null;
+}
 
 
 function game() {
 
 
-  var platform = document.getElementById('platform');   //platform object
-  oxo.animation.setPosition(platform, {x: 0, y: 550});
+  //player 1 jump
+  function jump() {
 
-  var player = document.getElementById('player');     //player object
-  oxo.animation.setPosition(player, {x: 600, y: 0});
+    if (ySpeed1 < 0) {
+      return;
+    }
 
-  
+    ySpeed1 = -7;
 
-  time = setInterval(down, gravity); 
+    setTimeout(function() {
+      ySpeed1 = 5;
+    }, 300
+    );
+    
+  }
+  //player 2 jump
+  function jump2() {
 
-  function down() {
+    if (ySpeed2 < 0) {
+      return;
+    }
 
-    // collision test
-    oxo.elements.onCollisionWithElement(player, platform, function() {
-      flying = false;
-      console.log('collision');
+    ySpeed2 = -7;
+
+    setTimeout(function() {
+      ySpeed2 = 5;
+    }, 500);
+    
+  }
+
+  var platforms;
+
+  //platform spawning
+  setInterval(() => {
+
+    var platform = oxo.elements.createElement({
+      type: 'div', // optional
+      class: 'game__platform game__platform--up', // optional,
+      styles: { // optional
+        transform: 'translate(' + oxo.utils.getRandomNumber(100, 1080) + 'px, ' + oxo.utils.getRandomNumber(50, 450) + 'px)'
+      },
+      appendTo: 'body' // optional
     });
 
-    if ( flying === true) {
-      oxo.animation.move(player, 'down', 10);
+    oxo.elements.onCollisionWithElement(player1, platform, function() {
+      if (oxo.animation.getPosition(player1).y + 40 <= oxo.animation.getPosition(platform).y) {
+        jump();
+      }});
+
+    oxo.elements.onCollisionWithElement(player2, platform, function() {
+      if (oxo.animation.getPosition(player2).y + 40 <= oxo.animation.getPosition(platform).y) {
+        jump2();
+      }});
+
+    oxo.elements.onLeaveScreenOnce(platform, function() {
+      platform.remove();
+    }, true);
+
+    platforms = document.querySelectorAll('.game__platform');
+    
+  }, platformSpawning);
+
+
+
+  var player2 = document.getElementById('player2');
+  var player1 = document.getElementById('player1'); 
+  var startingPlatform = document.getElementById('platform');
+  oxo.animation.setPosition(startingPlatform, {x: 0, y: 600});
+  oxo.animation.setPosition(player1, {x: 200, y: 500});
+  oxo.animation.setPosition(player2, {x: 850, y: 500});
+
+  var platformStart = document.getElementById('platform');
+  oxo.elements.onCollisionWithElement(player1, platformStart, jump);
+  oxo.elements.onCollisionWithElement(player2, platformStart, jump2);
+
+
+
+
+
+
+
+
+  //moving system
+
+  var pressed = [];
+
+
+  document.addEventListener('keydown', function(event) {
+    if (pressed.indexOf(event.key) === -1) {
+      pressed.push(event.key);
+    }
+  });
+  document.addEventListener('keyup', function(event) {
+    if (pressed.indexOf(event.key) >= 0) {
+      pressed = pressed.filter(key => key !== event.key);
+    }
+  });
+
+
+
+  setInterval(() => {
+
+    if (flying === true) {
+      oxo.animation.move(player1, 'down', ySpeed1, true);
+
+      oxo.animation.move(player2, 'down', ySpeed2, true);
+    }
+
+  }, gravity);
+
+
+
+  var player1Y;
+  var player2Y;
+
+
+  setInterval(function() {
+
+
+    // console.log('game is running');
+    // console.log(pressed);
+
+    if (pressed.indexOf('q') !== -1) {
+      oxo.animation.move(player2, 'left', movementSpeed);
+    };
+    if (pressed.indexOf('d') !== -1) {
+      oxo.animation.move(player2, 'right', movementSpeed);
+    };
+    if (pressed.indexOf('ArrowLeft') !== -1) {
+      oxo.animation.move(player1, 'left', movementSpeed);
+    };
+    if (pressed.indexOf('ArrowRight') !== -1) {
+      oxo.animation.move(player1, 'right', movementSpeed);
     };
 
-    oxo.inputs.listenArrowKeys(function(key) {
-      if ( key === 'right' ) {
-        oxo.animation.move(player, 'right', 10);
-      } else if (key === 'left') {
-        oxo.animation.move(player, 'left', 10);
-      } else if (key === 'up') {
-        oxo.animation.move(player, 'up', 200);
-        setTimeout(() => {
-          flying = true;
-        }, 300);
+    player1Y = oxo.animation.getPosition(player1);
+    player2Y = oxo.animation.getPosition(player2);
+
+
+    if (player1Y.y < player2Y.y) {
+      if (player1Y.y <= 200) {
+        for (let platform of platforms) {
+          oxo.animation.move(platform, 'down', (player1Y.y + 800) / 400, true)
+        }
       }
-    });
+    } else if (player2Y.y <= 200) {
+      for (let platform of platforms) {
+        oxo.animation.move(platform, 'down', (player2Y.y + 800) / 400, true)
+      }
+    };
+  }, gameSpeed);
 
-  };
-
-
-
-
-  // var ground = 0;
-
-  // while ( ground <= 0 ) {
-  //   oxo.animation.move(player, 'down', gravity);
-  //   ground = 0;
-  //   var ennemy = document.getElementById('ennemy');
-  //   oxo.elements.onCollisionWithElement(player, platform, function() {
-  //     ground = 1;
-  //   });
-  // }
-    
-
-
-  
-  
-  
-  // Not working prototype of moving and jumping
-
-  // oxo.inputs.listenArrowKeys(function(key) {
-  //   if ( key === 'right' ) {
-  //     oxo.animation.move(player, 'right', 3);
-  //   } else if (key === 'left') {
-  //     oxo.animation.move(player, 'left', 3);
-  //   } else if (key === 'up') {
-  //     oxo.animation.move(player, 'up', 200);
-  //     setTimeout( function() {
-  //       oxo.animation.move(player, 'down', 200);
-  //     }, 400);
-  //   }
-  // });
 };
