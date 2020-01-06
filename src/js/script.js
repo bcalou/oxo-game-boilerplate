867a4f0a9389d85e4f4a9f17b95b7f83eb1a6fbb
jason <you@example.com>
2020-01-06 17:44:38 +0100

    testcircle

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+var character = document.getElementById('character');
+oxo.animation.move(character, 'right', 100); // Move 10px to the right
+
+var character1 = document.getElementById('character1');
+oxo.animation.move(character1, 'left', 10); // Move 10px to the right
+
+
