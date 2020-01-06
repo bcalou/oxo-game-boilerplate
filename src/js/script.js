39e30fa08ab7cf681846fd85eacc64eb9164241d
jason <you@example.com>
2020-01-06 17:48:09 +0100

    pbjs

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 var character = document.getElementById('character');
 oxo.animation.move(character, 'right', 100); // Move 10px to the right
 
-var character1 = document.getElementById('character1');
-oxo.animation.move(character1, 'left', 10); // Move 10px to the right
+
 
 
