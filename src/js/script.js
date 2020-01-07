69493e6266d93d58e86595b561f8ab2750fbebf4
Hgo0123 <hcordillot@gmail.com>
2020-01-07 21:46:26 +0100

    resize frame for google chrome

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,8 @@ var beer = document.querySelector(".beer");
 function move() {
   let mousedownTime;
   var beer = document.querySelector(".beer");
-
-// calcule le temps que l'on reste appuyer sur la souris
+  var arrow = document.querySelector(".arrow");
+  // calcule le temps que l'on reste appuyer sur la souris
   beer.addEventListener("mousedown", () => {
     mousedownTime = new Date().getTime();
   });
@@ -13,20 +13,34 @@ function move() {
       timeDifference = (mouseupTime - mousedownTime) / 3; // transforme le temps d'appui en pixels
     oxo.animation.move(beer, "up", timeDifference, true);
   });
+  collision();
+}
+function addScorePoint() {
+  oxo.player.addToScore(5);
 }
 
 function collision() {
   var beer = document.querySelector(".beer");
   var table = document.querySelector(".table");
-  var elColision = oxo.elements.onCollisionWithElement(beer, table, function() {
-    alert("you win !!!");
-    oxo.animation.setPosition(beer, { x: 10, y: 0 });
+  var touch;
+
+  oxo.elements.onCollisionWithElement(beer, table, function() {
+    touch = true;
+    console.log("win");
+    oxo.animation.setPosition(beer, { x: 0, y: 0 });
+    addScorePoint();
   });
+  // if (!touch && beer.offsetTop < 100) {
+  //   console.log("lose");
+
+  //   oxo.animation.setPosition(beer, { x: 60, y: 0 });
+  // }
 }
+
+
 
 function game() {
   move();
-  collision();
 }
 
 oxo.inputs.listenKeyOnce("enter", function() {
