cb32b9f6555bb7da29005da20a733ad27a9fb286
Hgo0123 <hcordillot@gmail.com>
2020-01-08 14:56:00 +0100

    change table place

--- src/js/script.js
+++ src/js/script.js
@@ -11,6 +11,7 @@ function move() {
       timeDifference = (mouseupTime - mousedownTime) / 2; // transforme le temps d'appui en pixels
     oxo.animation.move(beer, "up", timeDifference, true);
     console.log(timeDifference);
+    console.log();
   });
 
   collision();
@@ -40,42 +41,16 @@ function collision() {
 }
 function createtable(x, y) {
   var element = oxo.elements.createElement({
-    type: "div", // optional
-    class: "table", // optional,
-    obstacle: true, // optional,
+    type: "div",
+    class: "table",
+    obstacle: true,
     styles: {
-      // optional
       position: "absolute",
-      // top: "300px",
-      // left: "780px"
       top: x + "px",
       left: y + "px"
     },
-    appendTo: ".frame" // optional
+    appendTo: ".frame"
   });
-}
-
-function rotateBeer() {
-  var beer = document.querySelector(".beer");
-  let angle = 0;
-  let direction = "right";
-  setInterval(function() {
-    console.log(direction, angle);
-    if (direction === "right") {
-      angle++;
-
-      if (angle > 120) {
-        direction = "left";
-      }
-    } else {
-      angle--;
-
-      if (angle < -120) {
-        direction = "right";
-      }
-    }
-    beer.style.transform = "rotate(" + angle + "deg)";
-  }, 1);
 }
 
 // add 5 point score when beer touch table
@@ -84,12 +59,12 @@ function addScorePoint() {
 }
 
 function game() {
-  rotateBeer();
   move();
-  createtable(300, 780);
-  createtable(150, 650);
-  createtable(150, 200);
-  createtable(300, 70);
+  createtable(100, 70);
+  createtable(100, 300);
+  createtable(100, 760);
+
+  arrowDegrees();
 }
 
 // oxo.inputs.listenKeyOnce("enter", function() {
